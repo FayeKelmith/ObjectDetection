7d71d5ede6af7cfe6b1c2c41d178bd8cd22dfee1
Faye Kelmith Rernyuy <fayekelmith@gmail.com>
2023-08-31 18:40:47 +0530

    working image detect

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from transformers import DetrImageProcessor, DetrForObjectDetection
 import torch 
 from PIL import Image 
 import app
+import random
 import cv2 
 from PIL import Image
 
@@ -9,11 +10,13 @@ from PIL import Image
 
 
 pic = cv2.imread('resources/photos/street.jpg')
-pic = cv2.resize(pic, (0, 0), fx = 0.25, fy = 0.25)
-video = cv2.VideoCapture('resources/videos/kitten.mp4')
- 
- #INFO: passing in images to process them in model.
-  
+pic = cv2.resize(pic, (0, 0), fx = 0.2, fy = 0.2)
+
+#INFO: passing in images to process them in model.
+def random_color():
+    levels = range(32,256,32)
+    return tuple(random.choice(levels) for _ in range(3))
+
 def detection(pic):
     processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50")
     model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50")
@@ -30,43 +33,36 @@ def detection(pic):
     #processing output, thresholding and fitting setting target sizes
     results = processor.post_process_object_detection(outputs,target_sizes=target_sizes,threshold=0.9)[0]
     
+    #List to store differentt colors in them
+    colors = {}
+    
     
     for score,label,box in zip(results["scores"], results["labels"],results["boxes"]):
-        #FIXME:
-        #Algorithm to generate different colors for different objects on scene
-        
-        
-        #to obtain dimensions of detected image
         box = [round(i,2) for i in box.tolist()]
         start = (int(box[0]),int(box[1]))
         end = (int(box[2]),int(box[3]))
-        cv2.rectangle(pic,start,end,(0,255,0),thickness=2)
-        cv2.putText(pic,model.config.id2label[label.item()],(int(box[0]),int(box[3])),cv2.FONT_HERSHEY_COMPLEX,fontScale=0.5,color=(255,255,255),thickness=1)
+        
+        if label not in colors:
+            col =  random_color()
+            colors[label] = col
+        #to obtain dimensions of detected image
+            cv2.rectangle(pic,start,end,col,thickness=2)
+        
+        cv2.putText(pic,model.config.id2label[label.item()],(int(box[0]),int(box[3])-5),cv2.FONT_HERSHEY_COMPLEX,fontScale=0.4,color=(255,255,255),thickness=1)
         # print(f'Detected {model.config.id2label[label.item()]} with confidence:' f'{round(score.item(),3)} at location {box}')
+    
     cv2.imshow('Street',pic)
     cv2.waitKey(0)
 
 #INFO: to process videos  
       
-def processing_video(video):
-    while True:
-        ret, frame = video.read()
-        #Checking for the end or if video not read correctly
-        if not ret:
-            break;
-        detection(frame)
-        if(0xFF == ord('q')):
-        #if (cv2.waitKey(1) & 0xFF == ord('q')):
-            break;
 
 
-#detection(pic)
-processing_video(video)
+detection(pic)
+
 #TODO: 
 # 1. Create algorithm to generate different colors for particular category of objects
 # 1.1 Position the boxes better than they are now, same for the text
-# 2. Test the video function and optimize it
-# 3. Check on how to optimize speed or when to introduce live video
 # 4. Connect it with streamlit
 # 5. Try deployment again
         
